42c7b26b7122102c5b10bba156f899219e96a701
ESBorum <205617455+ESBorum@users.noreply.github.com>
2026-04-14 15:56:15 +0200

    Refactor main method and add sayHello function
    Created sayHello method to print a name and age.
    Passed "Sebastian" and 19 to demonstrate functionality.

--- selvstudie_github_eksempel.java
+++ selvstudie_github_eksempel.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hej, jeg hedder Sebastian");
-        System.out.println("Jeg er 18 år");
+        sayHello("Sebastian", 19);
+    }
+
+    public static void sayHello(String navn, int alder) {
+        System.out.println("Hej, jeg hedder " + navn);
+        System.out.println("Jeg er " + alder + " år");
     }
 }
