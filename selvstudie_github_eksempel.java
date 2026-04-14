56eef6c2dc030d5cce387839cdd7f807c83d2d3c
ESBorum <205617455+ESBorum@users.noreply.github.com>
2026-04-14 15:52:44 +0200

    Update greeting message in Main class
    Added name and age output

--- selvstudie_github_eksempel.java
+++ selvstudie_github_eksempel.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hej");
+        System.out.println("Hej, jeg hedder Sebastian");
+        System.out.println("Jeg er 18 år");
     }
 }
